0060122c7cf0711edf917b859d6ab0dc051f41ec
ANeuronI <yoshinoarata37@gmail.com>
2024-07-21 03:05:46 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/SideBar";
 import SessionProvider from "@/components/SessionProvider";
-import { getServerSession, Session } from "next-auth";
+import { getServerSession } from "next-auth";
 import Login from "@/components/Login";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import GoogleProvider from "next-auth/providers/google";
 import TopBar from "@/components/TopBar";
 import ClientProvider from "@/components/ClientProvider";
+
+// Define the font
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,33 +19,40 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const session: Session | null = await getServerSession(authOptions);
+}) {
+  // Fetch session from the server using getServerSession with configured providers
+  const session = await getServerSession({
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={ session } >
-
+        <SessionProvider session={session}>
           {!session ? (
             <Login />
-          ):(
-
+          ) : (
             <main className="flex">
-              <div className= " bg-[#0f0f0f] h-screen w-[15%] max-w-[581px] overflow-y-auto md:min-w-[20rem]">
-               < SideBar />
-               </div>
+              <div className="bg-[#0f0f0f] h-screen w-[15%] max-w-[581px] overflow-y-auto md:min-w-[20rem]">
+                <SideBar />
+              </div>
               <ClientProvider />
-              <section className="bg-[#131313] max-h-screen flex-1 w-full ">
-              <TopBar />
+              <section className="bg-[#131313] max-h-screen flex-1 w-full">
+                <TopBar />
                 {children}
               </section>
             </main>
           )}
-
-            </SessionProvider>
-            </body>
-        </html>
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
